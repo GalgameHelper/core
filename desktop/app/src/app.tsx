3e11fd010a0877a2d703434f528daa1b5c048289
guanruihua <2116047353@qq.com>
2024-06-29 23:03:52 +0800

    chore: dev

--- desktop/app/src/app.tsx
+++ desktop/app/src/app.tsx
@@ -6,7 +6,12 @@ import { createRoot } from 'react-dom/client'
 const root = createRoot(document.body)
 
 function App() {
-  return <h2>Hello from React18!</h2>
+  return (
+    <div>
+      <div className='title'>标题</div>
+      <h2>Hello from React18!</h2>
+    </div>
+  )
 }
 
 root.render(<App />)
